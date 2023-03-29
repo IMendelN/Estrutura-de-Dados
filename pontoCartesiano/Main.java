54bdc46c8df21f51df05a5b4899155a506f798fe
Iago Mendes Nogueira <iagomwndes432@gmail.com>
2023-03-29 19:20:53 -0300

    PontoCartesianoVS

---
+++ pontoCartesiano/Main.java
@@ -0,0 +1,22 @@
+package pontoCartesiano;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escreva os valores de xA, yA e xB, yB respesctivamente: ");
+		double xA = sc.nextDouble();
+		double yA = sc.nextDouble();
+		double xB = sc.nextDouble();
+		double yB = sc.nextDouble();
+		
+		PontoCartesiano p = new PontoCartesiano(xA, yA, xB, yB);
+
+		System.out.println(p.distance());
+
+	}
+
+}
---
+++ pontoCartesiano/PontoCartesiano.java
@@ -0,0 +1,52 @@
+package pontoCartesiano;
+public class PontoCartesiano {
+    
+        private double xA, yA, xB, yB;
+    
+        public PontoCartesiano(double xA, double yA, double xB, double yB) {
+    
+            this.xA = xA;
+            this.yA = yA;
+            this.xB = xB;
+            this.yB = yB;
+        }
+    
+        public double getxA() {
+            return xA;
+        }
+    
+        public void setxA(double xA) {
+            this.xA = xA;
+        }
+    
+        public double getyA() {
+            return yA;
+        }
+    
+        public void setyA(double yA) {
+            this.yA = yA;
+        }
+    
+        public double getxB() {
+            return xB;
+        }
+    
+        public void setxB(double xB) {
+            this.xB = xB;
+        }
+    
+        public double getyB() {
+            return yB;
+        }
+    
+        public void setyB(double yB) {
+            this.yB = yB;
+        }
+    
+        public double distance() {
+            
+            return Math.sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
+    
+        }
+    }
+    
